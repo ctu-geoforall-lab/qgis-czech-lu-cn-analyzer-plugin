adb38c0e173df8011f54439f77cd7164be8a5489
Josef Jehlička <jehlijos@cvut.cz>
2025-03-03 20:25:03 +0100

    New classes created
    WFSdowloader - provides functions neede for dowloading via WFS
    
    WFStask - contains task that dowloads layers via WFS
    
    InputChecker- checks user and project inputs
    
    UIupdater - changes UI

---
+++ UIupdater.py
@@ -0,0 +1,120 @@
+# UIupdater.py
+
+from qgis.core import QgsMessageLog, Qgis
+from qgis.utils import iface
+from PyQt5.QtCore import pyqtSignal, QObject
+
+class UIUpdater(QObject):
+    """Class to update the UI elements during processing."""
+    progressChanged = pyqtSignal(int)
+
+    def __init__(self, run_button, progress_bar, abort_button, label, polygon_button, extent_button, polygon_label, mMapLayerComboBox):
+        super().__init__()
+        self.runButton = run_button
+        self.progressBar = progress_bar
+        self.abortButton = abort_button
+        self.label = label
+        self.polygonButton = polygon_button
+        self.extentButton = extent_button
+        self.polygonLabel = polygon_label
+        self.mMapLayerComboBox = mMapLayerComboBox
+        self.AreaFlag = False
+        self.plus_one_index = 0
+        self.wfs_layers = []
+
+    def ToggleChangeToPolygon(self):
+        """Toggle the computation to the polygon layer."""
+        self.AreaFlag = True
+        self.polygonButton.setChecked(False)
+        self.polygonLabel.setEnabled(True)
+        self.mMapLayerComboBox.setEnabled(True)
+
+    def ToggleChangeToExtent(self):
+        """Toggle the computation to the extent of the map canvas."""
+        self.AreaFlag = False
+        self.extentButton.setChecked(False)
+        self.polygonLabel.setEnabled(False)
+        self.mMapLayerComboBox.setEnabled(False)
+
+    def ErrorMsg(self, message):
+        """Display an error message."""
+        iface.messageBar().pushMessage("Error", message, level=Qgis.Critical, duration=5)
+
+    def LoadingMsg(self, msg):
+        """Display a loading message."""
+        iface.messageBar().pushMessage("Loading", msg, level=Qgis.Info, duration=0)
+
+    def CloseLoadingMsg(self):
+        """Close messages."""
+        iface.messageBar().clearWidgets()
+
+    def setButtonstoDefault(self):
+        """Set the UI buttons to their default state."""
+        self.runButton.setEnabled(True)
+        self.progressBar.setEnabled(False)
+        self.abortButton.setEnabled(False)
+        self.progressBar.setValue(0)
+        self.label.setText("")
+        self.polygonButton.setEnabled(True)
+        self.extentButton.setEnabled(True)
+
+    def freeze_ui(self):
+        """Freeze the UI elements during processing."""
+        self.runButton.setEnabled(False)
+        self.extentButton.setEnabled(False)
+        self.polygonButton.setEnabled(False)
+        self.abortButton.setEnabled(True)
+        self.progressBar.setEnabled(True)
+        self.progressBar.setValue(0)
+        self.label.setText("Downloading WFS layers...")
+
+    def reset_panel(self):
+        self.label.setStyleSheet("QLabel { color : black; }")  # Set the label color to black
+        self.progressBar.setValue(0)  # Reset progress bar
+
+    def updateProgressBar(self, value):
+        """Signaled by task - Update the progress bar value based on the task progress."""
+        self.progressBar.setValue(value)
+
+    def _reset_ui(self, message, progress_value):
+        """Reset the UI elements after task cancellation or completion."""
+        self.progressBar.setValue(progress_value)
+        self.runButton.setEnabled(True)
+        self.abortButton.setEnabled(False)
+        self.progressBar.setEnabled(False)
+        self.polygonButton.setEnabled(True)
+        self.extentButton.setEnabled(True)
+        self.label.setText(message)
+        iface.messageBar().clearWidgets()
+        iface.messageBar().pushMessage("Exiting", message, level=Qgis.Critical, duration=5)
+
+    def TaskCanceled(self):
+        """Signaled by task - Handle the cancellation of the processing task."""
+        iface.messageBar().clearWidgets()
+        iface.messageBar().pushMessage("Warning", "Process was canceled by user", level=Qgis.Warning, duration=5)
+        self._reset_ui("Task was canceled by user.", 0)
+        QgsMessageLog.logMessage("Task was canceled by user.","CzLandUseCN", level=Qgis.Info, notifyUser=False)
+
+    def TaskError(self, e):
+        """Signaled by task - Handle errors that occurred during the processing task."""
+        QgsMessageLog.logMessage(e,"CzLandUseCN",  level=Qgis.Critical, notifyUser=True)
+        iface.messageBar().pushMessage("ERROR", str(e), level=Qgis.Critical, duration=5)
+        self._reset_ui("Error occurred during processing.", 0)
+
+    def TaskSuccess(self):
+        """ Handle the successful completion of the processing task."""
+        QgsMessageLog.logMessage("Editing layers.", "CzLandUseCN", level=Qgis.Info, notifyUser=False)
+        self.label.setText("Editing ZABAGED layers...")
+
+    def PluginSuccess(self):
+        """ Modify the UI elements after task completion."""
+        QgsMessageLog.logMessage("Success!", "CzLandUseCN", level=Qgis.Info, notifyUser=False)
+        self.progressBar.setValue(100)
+        iface.messageBar().clearWidgets()
+        self.runButton.setEnabled(True)
+        self.abortButton.setEnabled(False)
+        self.label.setStyleSheet("QLabel { color : green; }")
+        self.label.setText("Completed :)")
+        self.polygonButton.setEnabled(True)
+        self.extentButton.setEnabled(True)
+        iface.messageBar().pushMessage("Success", "Task completed successfully", level=Qgis.Success, duration=5)
---
+++ WFSdownloader.py
@@ -0,0 +1,117 @@
+from qgis.core import QgsMessageLog, Qgis, QgsVectorLayer, QgsRectangle, QgsGeometry, QgsFeature, QgsFeatureRequest, \
+    QgsWkbTypes
+from qgis.utils import iface
+import processing
+from typing import Optional, List
+
+
+class WFSDownloaderError(Exception):
+    pass
+
+
+class WFSDownloader:
+    """ Class to download and clip WFS layers, used before and during WFStask """
+
+    def __init__(self, conf_path, area_flag, polygon):
+        self.config_path = conf_path
+        self.AreaFlag = area_flag
+        self.polygon = polygon
+
+    def get_ZABAGED_layers_list(self) -> List[str]:
+        """ Load WFS layers from the configuration file"""
+        try:
+            with open(self.config_path, mode='r', encoding='utf-8') as file:
+                lines = file.readlines()
+
+            processed_lines = [line.strip() for line in lines if line.strip() != "LPIS_layer"]
+            return processed_lines
+        except Exception as e:
+            QgsMessageLog.logMessage(f"Failed to load WFS layers (layers_merging_order.csv): {e}", "CzLandUseCN",
+                                     level=Qgis.Warning, notifyUser=True)
+            return []
+
+    def clip_layer(self, layer: QgsVectorLayer, extent: QgsRectangle, layer_name: str) -> QgsVectorLayer:
+        """ Clip the layer to the given extent"""
+        extent_geom = QgsGeometry.fromRect(extent)
+        clipped_layer = QgsVectorLayer(
+            f"{QgsWkbTypes.displayString(layer.wkbType())}?crs={layer.crs().authid()}",
+            layer_name,
+            "memory"
+        )
+
+        clipped_layer.dataProvider().addAttributes(layer.fields())
+        clipped_layer.updateFields()
+
+        for feature in layer.getFeatures(QgsFeatureRequest().setFilterRect(extent)):
+            geom = feature.geometry()
+            if geom.intersects(extent_geom):
+                clipped_feature = QgsFeature()
+                clipped_feature.setGeometry(geom.intersection(extent_geom))
+                clipped_feature.setAttributes(feature.attributes())
+                clipped_layer.dataProvider().addFeature(clipped_feature)
+
+        clipped_layer.commitChanges()
+
+        return clipped_layer
+
+    def get_wfs_info(self, wfs_layers):
+        """ Get WFS layers and extent information"""
+        if not wfs_layers:
+            QgsMessageLog.logMessage("Corupted WFS setting, see config file", "CzLandUseCN",
+                                     level=Qgis.Warning, notifyUser=True)
+            return
+
+        if not self.AreaFlag:
+            extent = iface.mapCanvas().extent()
+        elif self.polygon and self.polygon.isValid():
+            extent = self.polygon.extent()
+        else:
+            QgsMessageLog.logMessage("Invalid polygon layer!", "CzLandUseCN", level=Qgis.Warning, notifyUser=True)
+
+        return extent.yMinimum(), extent.xMinimum(), extent.yMaximum(), extent.xMaximum(), extent
+
+    def process_wfs_layer(self, layer_name: str, ymin: float, xmin: float, ymax: float, xmax: float,
+                          extent: QgsGeometry,
+                          URL: str) -> Optional[QgsVectorLayer]:
+        """ Load and clip a WFS layer to the given extent"""
+        uri = (
+            f"{URL}?"
+            f"&version=2.0.0&request=GetFeature&typename={layer_name}"
+            f"&bbox={xmin},{ymin},{xmax},{ymax},EPSG:5514"
+        )
+
+        vlayer = QgsVectorLayer(uri, f"Layer: {layer_name}", "WFS")
+        if not vlayer.isValid() or not vlayer.featureCount() or vlayer is None:
+            QgsMessageLog.logMessage(f"Failed to load or empty layer: {layer_name}", "CzLandUseCN",
+                                     level=Qgis.Warning, notifyUser=True)
+            return
+
+        clipped_layer = self.clip_layer(vlayer, extent, layer_name)
+        if clipped_layer.isValid():
+            return clipped_layer
+
+    def ClipByPolygon(self, layer: QgsVectorLayer) -> QgsVectorLayer:
+        """ Clip the layer to the polygon extent"""
+        params = {
+            'INPUT': layer,
+            'OVERLAY': self.polygon,
+            'OUTPUT': 'memory:'
+        }
+        clipped_result = processing.run("native:clip", params)
+        final_clipped_layer = clipped_result['OUTPUT']
+
+        # Add the clipped layer to the map if it contains features
+        if final_clipped_layer and final_clipped_layer.featureCount() > 0:
+            final_clipped_layer.setName(layer.name())
+            return final_clipped_layer  # Return the clipped layer
+
+    def GetLPISLayer(self, LPISURL: str, layer_name: str, LPISconfigpath: str, ymin: float, xmin: float, ymax: float,
+                     xmax: float, current_extent: QgsGeometry, LayerList: list) -> list:
+        """ Get the LPIS layer from the WFS service"""
+        wfs_downloader = WFSDownloader(LPISconfigpath, self.AreaFlag, self.polygon)
+        LPISlayer = wfs_downloader.process_wfs_layer(layer_name, ymin, xmin, ymax, xmax, current_extent, LPISURL)
+        if LPISlayer is None:
+            QgsMessageLog.logMessage("Unavailable LPIS Layer", "CzLandUseCN", level=Qgis.Warning, notifyUser=True)
+            return LayerList
+        LPISlayer.setName("LPIS_layer")
+        return LayerList
